db383a644964543a3a28173e2a81d16efed22a6c
Anders Einar Hilden <hildenae@gmail.com>
2019-04-23 00:47:13 +0200

    Cleanup utils.full_check, removing signal timout handling

--- checkdmarc-flask/utils.py
+++ checkdmarc-flask/utils.py
@@ -9,23 +9,14 @@ from datetime import timezone, timedelta
 
 # We want to skip tls check (port 25 etc traffic) by default when running on Heroku
 def full_check(domain, skip_tls=True, timeout=None):
-    res = None
-    nameservers=os.getenv('NAMESERVERS', None)
+    nameservers = os.getenv('NAMESERVERS', None)
     if nameservers:
         nameservers = nameservers.split(",")
-    if timeout:
-        def raise_TimedOutExc(a, b):
-            raise TimedOutExc(f"checkdmarc used more than predefined timeout ({timeout} seconds)")
-        signal.signal(signal.SIGALRM, raise_TimedOutExc)
-        signal.alarm(timeout)
+    res = checkdmarc.check_domains([domain], skip_tls=skip_tls, nameservers=nameservers)
 
-        res = checkdmarc.check_domains([domain], skip_tls=skip_tls, nameservers=nameservers)
-        signal.alarm(0)
-    else:
-        res = checkdmarc.check_domains([domain], skip_tls=skip_tls, nameservers=nameservers)
     output = OrderedDict()
     output['_about'] = "For questions: mailto:hildenae+dmarc@gmail.com. Data produced "\
-                      "using https://domainaware.github.io/checkdmarc/index.html"
+                       "using https://domainaware.github.io/checkdmarc/index.html"
     output['_version'] = f"checkdmarc {checkdmarc.__version__} (from pip)"
     output['_nameservers'] = nameservers
     # We do this to make sure "about" is put first in the merged ordered dict
@@ -40,6 +31,6 @@ def force_iso_tz(timestamp):
     # Set a timezone offset of 0 to force printing timezone even if GMT
     return timestamp.replace(microsecond=0).replace(tzinfo=timezone(timedelta(0))).isoformat()
 
+
 class TimedOutExc(Exception):
     pass
-
